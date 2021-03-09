9012d127232c4aad7d0771da5c053905163faa13
Giuliano <giuliano.brunero@gmail.com>
2021-03-09 20:30:49 -0300

    Primer commit, puesta en marcha del proyecto

---
+++ mergesort.py
@@ -0,0 +1,9 @@
+# Solución
+mergesort = lambda l1, l2: sorted(l1 + l2)
+
+# Prueba
+'''
+l1 = ['Cristiano', 'Drogba', 'Kaka', 'Leo', 'Lukaku']
+l2 = ['Armani', 'Bebeto', 'Mascherano', 'Rojo']
+print(mergesort(l1, l2))
+'''

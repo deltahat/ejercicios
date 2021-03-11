bae6fa92afd1dcd760a0487d14d1298a9b299514
deltahat <giuliano.brunero@gmail.com>
2021-03-11 15:03:52 -0300

    Endpoint para subir archivos listo

--- csvuploader/core/views.py
+++ csvuploader/core/views.py
@@ -6,12 +6,23 @@ from rest_framework import status
 from .serializers import FileSerializer
 
 class FileView(APIView):
-  parser_classes = (MultiPartParser, FormParser)
+	parser_classes = (MultiPartParser, FormParser)
 
-  def post(self, request, *args, **kwargs):
-    file_serializer = FileSerializer(data=request.data)
-    if file_serializer.is_valid():
-      file_serializer.save()
-      return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-    else:
-      return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	def post(self, request, *args, **kwargs):
+		file_serializer = FileSerializer(data=request.data)
+		if file_serializer.is_valid():
+			file_serializer.save()
+			context = file_serializer.data
+			context['file_url'] = context.pop('file')
+			return Response(context, status=status.HTTP_201_CREATED)
+		else:
+			return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ListFiles(APIView):
+
+	def get(self, request, *args, **kwargs):
+		files = File.objects.all()
+		context = {}
+		return Response(files, status=status.HTTP_201_CREATED)

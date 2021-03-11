88d87b00b67b2c9e494d4f1031d0893c3bc072a2
deltahat <giuliano.brunero@gmail.com>
2021-03-11 15:30:15 -0300

    Endpoint para obtener el listado de los archivos subidos listo

--- csvuploader/core/urls.py
+++ csvuploader/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import FileView
+from .views import FileView, ListFiles
 
 urlpatterns = [
 	path('upload/', FileView.as_view(), name = 'file-upload'),
+	path('csv-history/', ListFiles.as_view(), name = 'list-files'),
 ]
--- csvuploader/core/views.py
+++ csvuploader/core/views.py
@@ -4,6 +4,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FileSerializer
+from .models import File
 
 class FileView(APIView):
 	parser_classes = (MultiPartParser, FormParser)
@@ -21,8 +22,6 @@ class FileView(APIView):
 
 
 class ListFiles(APIView):
-
 	def get(self, request, *args, **kwargs):
-		files = File.objects.all()
-		context = {}
-		return Response(files, status=status.HTTP_201_CREATED)
+		context =  [{files.file.name, files.file.url, files.timestamp} for files in File.objects.all()]
+		return Response((context), status=status.HTTP_201_CREATED)
